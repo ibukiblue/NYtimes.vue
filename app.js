70c8935fd6048ef09f9e55ce4cd2aec023ffbb85
ibukiblue <blueandchacha@gmail.com>
2019-03-05 20:23:54 +0900

    first-commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const NYTBaseUrl = "https://api.nytimes.com/svc/topstories/v2/";
+const ApiKey = "VH7xNMeUEQxs9anDuQoc1Tvt4qpWVQGg";
+
+function buildUrl (url) {
+  return NYTBaseUrl + url + ".json?api-key=" + ApiKey
+}
+
+const vm = new Vue ({
+  el: "#app",
+  data: {
+    results: []
+  },
+  mounted() {
+    this.getPosts('home');
+  },
+  methods: {
+    getPosts(section){ 
+      let url = buildUrl(section);
+      axios.get(url).then((response) => {
+        this.results = response.data.results;
+      }).catch(error => {console.log(error)});
+    }
+  }
+})
